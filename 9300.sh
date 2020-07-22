46f1beefc448a15ef5b5057b1e70df1752b58f04
JElchison <JElchison@users.noreply.github.com>
2020-07-21 20:46:40 -0400

    Add files via upload

---
+++ 9300.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -eufx -o pipefail
+
+
+sudo sh -c 'cat > /etc/apt/sources.list.d/focal-dell.list << EOF
+deb http://dell.archive.canonical.com/updates/ focal-dell public
+# deb-src http://dell.archive.canonical.com/updates/ focal-dell public
+
+deb http://dell.archive.canonical.com/updates/ focal-oem public
+# deb-src http://dell.archive.canonical.com/updates/ focal-oem public
+
+deb http://dell.archive.canonical.com/updates/ focal-somerville public
+# deb-src http://dell.archive.canonical.com/updates/ focal-somerville public
+
+deb http://dell.archive.canonical.com/updates/ focal-somerville-melisa public
+# deb-src http://dell.archive.canonical.com/updates focal-somerville-melisa public
+EOF'
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys F9FDA6BED73CDC22
+sudo apt update -qq
+
+sudo apt install oem-somerville-melisa-meta libfprint-2-tod1-goodix oem-somerville-meta tlp-config -y
+
+sudo add-apt-repository ppa:boltgolt/howdy -y
+sudo apt update -qq
+sudo apt install howdy -y
+
